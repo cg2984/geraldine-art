2d1900059e1e7ec84eac0d96d0e08bc4190428af
Clara <cg2984@nyu.edu>
2020-12-22 00:21:28 -0500

    made the footer. will add other things later

---
+++ website/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Footer from "./components/footer.js"
+import Main from "./components/main.js"
+import React from "react";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ website/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Footer() {
+  return (
+  	<footer>
+    	<h1>Geraldine Pinzon</h1>
+    	<ul>
+      		<li><a href="https://instagram.com" target="_blank" rel="noopener">instagram</a></li>
+      		<li><a href="/about" target="_blank" rel="noopener">about</a></li>
+    	</ul>
+  	</footer>
+  );
+}
+
+export default Footer;

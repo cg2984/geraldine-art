f678c9d498b296dfac8a9366a055a0c4221035d2
Clara <cg2984@nyu.edu>
2020-12-22 12:40:19 -0500

    did some of the styling and got the pictures in it

--- website/src/components/gallery.js
+++ website/src/components/gallery.js
@@ -3,8 +3,8 @@ import HorizontalScroll from 'react-scroll-horizontal'
 
 class Gallery extends Component{
 	render(){
-		const child = {width:`30em`, height:`30em`, backgroundColor:`#e6b52e`, margin:`10px`}
-		const parent = {width:`1080px`, height:`500px`}
+		const child = {width:`100em`, height:`100em`, backgroundColor:`#e6b52e`, margin:`10px`, backgroundImage:`url(https://upload.wikimedia.org/wikipedia/commons/d/d9/Collage_of_Nine_Dogs.jpg)`}
+		const parent = {width:`1080px`, height:`800px`}
 		return(
 			<div style={parent}>
 				<HorizontalScroll>
